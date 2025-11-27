8fe64c86b3dea4c90e6dabb01f1be8d9ecddfdaa
Sanjna Sisodiya <sanjanasisodiya782@gmail.com>
2025-11-28 00:23:03 +0530

    update

--- src/routes/scannerRoutes.js
+++ src/routes/scannerRoutes.js
@@ -9,7 +9,12 @@ import { authenticate, authorize } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 //verify ticket
-router.get("/verifyTicket/:ticketId", authenticate, verifyTicket);
+router.get(
+  "/verifyTicket/:ticketId",
+  authenticate,
+  authorize("admin", "organizer", "ticketChecker"),
+  verifyTicket
+);
 
 //Check-in API (atomic, prevents race conditions)
 router.post(
